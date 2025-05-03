a890ed2b3b0477eb48c965aa2ca80f15fd260f13
Gaurang Gujrati <gujratigaurang@gmail.com>
2025-05-03 21:36:16 +0530

    removed debate

--- app/components/BlendResult.jsx
+++ app/components/BlendResult.jsx
@@ -14,7 +14,6 @@ export default function BlendResult({ result }) {
     summary,
     user1,
     user2,
-    debateQuestion,
     conversation,
     blendPersona,
     irlPotentials,
@@ -269,17 +268,6 @@ export default function BlendResult({ result }) {
         </div>
       </div>
       
-      <div className="bg-white p-6 rounded-lg shadow-md border border-gray-200 mb-10">
-        <h2 className="text-xl font-semibold mb-4 text-black flex items-center font-mono tracking-tight">
-          <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
-          </svg>
-          Debate Question
-        </h2>
-        <div className="bg-gray-50 p-5 rounded-lg border border-gray-100">
-          <p className="text-gray-800 font-mono">{debateQuestion}</p>
-        </div>
-      </div>
       
       <div className="bg-white p-6 rounded-lg shadow-md border border-gray-200 mb-10">
         <h2 className="text-xl font-semibold mb-4 text-black flex items-center font-mono tracking-tight">
